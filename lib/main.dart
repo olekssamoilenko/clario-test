ea27c872ff78d9c03adc8284209d6415418fa583
oleksandr.samoilenko <oleksandr.samoilenko@extrawest.com>
2024-11-03 01:39:55 +0200

    add assets, create pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
 
 void main() {
   runApp(const MyApp());
@@ -12,10 +10,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: Placeholder()
-    );
+    return MaterialApp(title: 'Flutter Demo', home: Placeholder());
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:clario_test/main.dart';
+import 'package:form_demo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
