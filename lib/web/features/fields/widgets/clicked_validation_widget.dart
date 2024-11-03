e8b9b037720d9a8ad082fd8c3a0c9713a01dcb85
oleksandr.samoilenko <oleksandr.samoilenko@extrawest.com>
2024-11-03 18:51:12 +0200

    add web text sections, password validation sections

---
+++ lib/web/features/fields/widgets/clicked_validation_widget.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../../common/bloc/validation_bloc/validation_bloc.dart';
+import '../../../../common/consts/color_consts.dart';
+import '../../../../features/sign_in/widgets/validator_status_widget.dart';
+import '../../../../theme/assets.dart';
+
+
+class ClickedValidationWidget extends StatefulWidget {
+  const ClickedValidationWidget({super.key});
+
+  @override
+  State<ClickedValidationWidget> createState() => _ClickedValidationWidget();
+}
+
+class _ClickedValidationWidget extends State<ClickedValidationWidget> {
+  bool _obscurePassword = true;
+
+  @override
+  void initState() {
+    ///Need this to show error right after form init
+    context.read<ValidationBloc>().add(const PasswordChangedEvent(passwordValue: 'example@.com1'));
+    context.read<ValidationBloc>().add(FormSubmitted());
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ValidationBloc, ValidationState>(builder: (context, state) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextFormField(
+            initialValue: 'example@.com1',
+            obscureText: _obscurePassword,
+            onChanged: (password) {
+              context.read<ValidationBloc>().add(PasswordChangedEvent(passwordValue: password));
+            },
+            decoration: InputDecoration(
+              hintText: "Login",
+              fillColor: redLight1,
+              filled: true,
+              enabledBorder: _buildOutlineInputBorder(),
+              focusedBorder: _buildOutlineInputBorder(),
+              suffixIcon: IconButton(
+                icon: SvgPicture.asset(
+                  _obscurePassword ? hidePassword : showPassword,
+                ),
+                onPressed: () {
+                  setState(() {
+                    _obscurePassword = !_obscurePassword;
+                  });
+                },
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          const ValidationStatusWidget(),
+          const SizedBox(height: 40),
+        ],
+      );
+    });
+  }
+
+  OutlineInputBorder _buildOutlineInputBorder() {
+    return const OutlineInputBorder(
+      borderSide: BorderSide(color: redLight, style: BorderStyle.solid, width: 2),
+      borderRadius: BorderRadius.all(Radius.circular(30)),
+    );
+  }
+}
---
+++ lib/web/features/fields/widgets/realtime_validation_widget.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../../common/bloc/validation_bloc/validation_bloc.dart';
+import '../../../../features/sign_in/widgets/validator_status_widget.dart';
+import '../../../../theme/assets.dart';
+
+
+
+class RealtimeValidationWidget extends StatefulWidget {
+  const RealtimeValidationWidget({super.key});
+
+  @override
+  State<RealtimeValidationWidget> createState() => _RealtimeValidationWidget();
+}
+
+class _RealtimeValidationWidget extends State<RealtimeValidationWidget> {
+  bool _obscurePassword = true;
+
+  @override
+  void initState() {
+    context.read<ValidationBloc>().add(PasswordChangedEvent(passwordValue: 'example@.com1'));
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ValidationBloc, ValidationState>(builder: (context, state) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextFormField(
+            initialValue: 'example@.com1',
+            obscureText: _obscurePassword,
+            onChanged: (password) {
+              context.read<ValidationBloc>().add(PasswordChangedEvent(passwordValue: password));
+            },
+            decoration: InputDecoration(
+              hintText: "Login",
+              suffixIcon: IconButton(
+                icon: SvgPicture.asset(
+                  _obscurePassword ? hidePassword : showPassword,
+                ),
+                onPressed: () {
+                  setState(() {
+                    _obscurePassword = !_obscurePassword;
+                  });
+                },
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          const ValidationStatusWidget(),
+          const SizedBox(height: 40),
+        ],
+      );
+    });
+  }
+}
